54330d34ef83ce0559b68c3592533355ce318f42
oussamaZAGHDOUD <oussamazaghdoud1991@gmail.com>
2021-04-22 04:02:11 +0100

    first commit

---
+++ src/main/java/com/oussama/SoapWebServiceInvocationThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.oussama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceInvocationThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceInvocationThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oussama/SoapWebServiceInvocationThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebServiceInvocationThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
