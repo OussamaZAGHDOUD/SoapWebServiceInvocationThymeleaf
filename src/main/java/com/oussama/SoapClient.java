fce5a8e4b5182fcc3539461bfdcff83c0ea3429c
oussamaZAGHDOUD <oussamazaghdoud1991@gmail.com>
2021-04-22 23:44:36 +0100

    changing the emplacement of the SoapClient class to an independent package

--- src/main/java/com/oussama/SoapClient.java
+++
@@ -1,5 +0,0 @@
-package com.oussama;
-
-public class SoapClient {
-
-}
---
+++ src/main/java/com/oussama/client/SoapClient.java
@@ -0,0 +1,22 @@
+package com.oussama.client;
+
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import com.oussama.registrationEligibility.StudentRequest;
+import com.oussama.registrationEligibility.WsResponse;
+// the main role of SoapClient class is the invokation of our service 
+@Service
+public class SoapClient {
+
+	private WebServiceTemplate serviceTemplate;
+	private Jaxb2Marshaller marshaller;
+	
+	public WsResponse getStudentStatus(StudentRequest request) {
+		serviceTemplate = new WebServiceTemplate(marshaller);
+		// invoke the service
+		WsResponse response = (WsResponse) serviceTemplate.marshalSendAndReceive("http://localhost:9090/ws",request);
+		return response;
+	}
+}
